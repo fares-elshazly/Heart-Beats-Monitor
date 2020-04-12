eed26593a8834b4d1951c8be77cc01fb637a1a70
Fares El-Shazly <fares.elshazlyy@gmail.com>
2020-04-12 13:14:23 +0200

    New Algorithm

--- lib/BPMScreen.dart
+++ lib/BPMScreen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:hello_heart/ImageProcessing.dart';
-import 'package:torch/torch.dart';
+
+var result = -1;
+var averageIndex = 0;
+final averageArraySize = 4;
+List<int> averageArray = List<int>.filled(averageArraySize, 0);
+enum TYPE { GREEN, RED }
+TYPE currentType = TYPE.GREEN;
+TYPE getCurrent() {
+  return currentType;
+}
+
+var beatsIndex = 0;
+final beatsArraySize = 3;
+List<int> beatsArray = List<int>.filled(beatsArraySize, 0);
+double beats = 0;
+var startTime = 0;
 
 class BPMScreen extends StatefulWidget {
   final CameraDescription camera;
@@ -40,7 +55,7 @@ class _BPMScreenState extends State<BPMScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Heart Beat Test')),
+      appBar: AppBar(title: Text('Heart Beat AVG: $result')),
       body: FutureBuilder<void>(
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
@@ -55,19 +70,102 @@ class _BPMScreenState extends State<BPMScreen> {
         child: Icon(Icons.timer),
         onPressed: () async {
           _controller.flash(true);
-          if (await Torch.hasTorch) Torch.turnOn();
           ImageProcessing imageProcessing = ImageProcessing();
           stopWatch.start();
+
           _controller.startImageStream((CameraImage image) async {
-            imageProcessing.addCameraFrame(image);
+            // imageProcessing.addCameraFrame(image);
+            // if (stopWatch.elapsed.inSeconds > 10) {
+            //   _controller.stopImageStream();
+            //   if (await Torch.hasTorch) Torch.turnOff();
+            //   int bpm = await imageProcessing.calculateBPM(stopWatch.elapsed.inSeconds);
+            //   print('Heart Beats Per Minute: $bpm');
+            //   stopWatch.stop();
+            //   stopWatch.reset();
+            //   Navigator.of(context).pop();
+            // }
+            int width = image.width;
+            int height = image.height;
+            print('New Image');
+            print('Time: ${stopWatch.elapsed.inSeconds}');
+
+            int imgAvg = imageProcessing.getImageColors(image)[2].round();
+            print('ImageAvg: $imgAvg');
+            if (imgAvg == 0 || imgAvg == 255) {
+              print('Image Not Valid!');
+              return;
+            }
+
+            int averageArrayAvg = 0;
+            int averageArrayCnt = 0;
+            print(averageArray[0]);
+            for (int i = 0; i < averageArray.length; i++) {
+              if (averageArray[i] > 0) {
+                averageArrayAvg += averageArray[i];
+                averageArrayCnt++;
+              }
+            }
+
+            int rollingAverage =
+                (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt).round() : 0;
+            TYPE newType = currentType;
+            if (imgAvg < rollingAverage) {
+              newType = TYPE.RED;
+              if (newType != currentType) {
+                beats++;
+                print('BEAT!! Beats = $beats');
+                // Log.d(TAG, "BEAT!! beats="+beats);
+              }
+            } else if (imgAvg > rollingAverage) {
+              newType = TYPE.GREEN;
+            }
+
+            if (averageIndex == averageArraySize) averageIndex = 0;
+            averageArray[averageIndex] = imgAvg;
+            averageIndex++;
+
+            if (newType != currentType) {
+              currentType = newType;
+            }
+
+            int totalTimeInSecs = stopWatch.elapsed.inSeconds;
             if (stopWatch.elapsed.inSeconds > 10) {
+              print('If Condition');
               _controller.stopImageStream();
-              if (await Torch.hasTorch) Torch.turnOff();
-              int bpm = await imageProcessing.calculateBPM(stopWatch.elapsed.inSeconds);
-              print('Heart Beats Per Minute: $bpm');
-              stopWatch.stop();
+              _controller.flash(false);
+              print('Stopping Image Streeeeam!');
+              double bps = (beats / totalTimeInSecs);
+              int dpm = (bps * 60).round();
+              if (dpm < 30 || dpm > 180) {
+                stopWatch.reset();
+                beats = 0;
+                print('DPM: $dpm');
+                return;
+              }
+
+              // Log.d(TAG,
+              // "totalTimeInSecs="+totalTimeInSecs+" beats="+beats);
+              print('Total Time In Seconds: $totalTimeInSecs And Beats: $beats');
+
+              if (beatsIndex == beatsArraySize) beatsIndex = 0;
+              beatsArray[beatsIndex] = dpm;
+              beatsIndex++;
+
+              int beatsArrayAvg = 0;
+              int beatsArrayCnt = 0;
+              for (int i = 0; i < beatsArray.length; i++) {
+                if (beatsArray[i] > 0) {
+                  beatsArrayAvg += beatsArray[i];
+                  beatsArrayCnt++;
+                }
+              }
+              int beatsAvg = (beatsArrayAvg / beatsArrayCnt).round();
+              print(beatsAvg);
+              setState(() {
+                result = beatsAvg;
+              });
               stopWatch.reset();
-              Navigator.of(context).pop();
+              beats = 0;
             }
           });
         },
--- lib/ImageProcessing.dart
+++ lib/ImageProcessing.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'package:camera/camera.dart';
 import 'package:smart_signal_processing/smart_signal_processing.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class ImageProcessing {
   double redBlueRatio = 0;
@@ -64,7 +65,8 @@ class ImageProcessing {
     double samplingFrequency = (redAvgList.length / totalTimeInSeconds);
 
     List redAvgListCopy = redAvgList.map((element) => element).toList();
-    double frequency = await calculateFrequency(redAvgListCopy, samplingFrequency);
+    double frequency =
+        await calculateFrequency(redAvgListCopy, samplingFrequency);
 
     int bpm = (frequency * 60).ceil();
 
@@ -124,4 +126,63 @@ class ImageProcessing {
 
     return v;
   }
+
+  // int decodeYUV420SPtoRedSum(CameraImage image) {
+  //   if (image == null) return 0;
+
+  //   int width = image.width;
+  //   int height = image.height;
+  //   int imageSize = width * height;
+  //   int sum = 0;
+
+  //   print('Image Plan 0: ${image.planes[0].bytes.length}');
+  //   print('Image Plan 1: ${image.planes[1].bytes.length}');
+  //   print('Image Plan 2: ${image.planes[2].bytes.length}');
+
+  //   for (int j = 0, yp = 0; j < height; j++) {
+  //     int uvp = imageSize + (j >> 1) * width, u = 0, v = 0;
+  //     print('UVP: $uvp');
+  //     // for (int i = 0; i < width; i++, yp++) {
+  //     //   print('UVP: $uvp');
+  //     //   print('YP: $yp');
+  //     //   int y = (0xff & image.planes[0].bytes[yp]) - 16;
+  //     //   if (y < 0) y = 0;
+  //     //   if ((i & 1) == 0) {
+  //     //     v = (0xff & image.planes[2].bytes[uvp++]) - 128;
+  //     //     u = (0xff & image.planes[1].bytes[uvp++]) - 128;
+  //     //   }
+  //     //   int y1192 = 1192 * y;
+  //     //   int r = (y1192 + 1634 * v);
+  //     //   int g = (y1192 - 833 * v - 400 * u);
+  //     //   int b = (y1192 + 2066 * u);
+
+  //     //   if (r < 0)
+  //     //     r = 0;
+  //     //   else if (r > 262143) r = 262143;
+  //     //   if (g < 0)
+  //     //     g = 0;
+  //     //   else if (g > 262143) g = 262143;
+  //     //   if (b < 0)
+  //     //     b = 0;
+  //     //   else if (b > 262143) b = 262143;
+
+  //     //   int pixel = 0xff000000 |
+  //     //       ((r << 6) & 0xff0000) |
+  //     //       ((g >> 2) & 0xff00) |
+  //     //       ((b >> 10) & 0xff);
+  //     //   int red = (pixel >> 16) & 0xff;
+  //     //   sum += red;
+  //     // }
+  //   }
+  //   return sum;
+  // }
+
+  // int decodeYUV420SPtoRedAvg(CameraImage image) {
+  //   if (image == null) return 0;
+
+  //   int frameSize = image.width * image.height;
+
+  //   int sum = decodeYUV420SPtoRedSum(image);
+  //   return (sum / frameSize).round();
+  // }
 }
